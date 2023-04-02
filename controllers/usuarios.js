1230fce1ea8d2aa57a056fc5aa6bbb0ae1a147a1
Hugo <hugo@boahstudio.com>
2023-04-02 14:00:15 -0300

    CRUD

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -3,17 +3,21 @@ const bcryptjs = require('bcryptjs')
 const Usuario = require('../models/usuario')
 
 
-const usuariosGet = (req = request, res = response) => {
+const usuariosGet = async(req = request, res = response) => {
 
-    const { q, nombre = 'No name', apikey, page = 1, limit } = req.query
-    
+    const { limite = 5, desde = 0 } = req.query
+    const query = { estado: true }
+
+    const [ total, usuarios ] = await Promise.all([
+        Usuario.countDocuments(query),
+        Usuario.find(query)
+            .skip(Number( desde ))
+            .limit(Number( limite ))
+    ])
+
     res.json({
-        msg: 'get API - controlador',
-        q,
-        nombre,
-        apikey,
-        page,
-        limit
+        total,
+        usuarios
     })
 }
 
@@ -34,14 +38,20 @@ const usuariosPost = async(req, res =  response) => {
     })
 }
 
-const usuariosPut = (req, res = response) => {
+const usuariosPut = async(req, res = response) => {
 
     const { id } = req.params
+    const { password, google, correo, ...resto } = req.body
 
-    res.json({
-        msg: 'put API - controlador',
-        id
-    })
+    if ( password ) {
+         // Encriptar la contraseña
+        const salt = bcryptjs.genSaltSync()
+        resto.password = bcryptjs.hashSync( password, salt )
+    }
+
+    const usuario = await Usuario.findByIdAndUpdate( id, resto )
+
+    res.json(usuario)
 }
 
 const usuariosPatch = (req, res = response) => {
@@ -50,10 +60,17 @@ const usuariosPatch = (req, res = response) => {
     })
 }
 
-const usuariosDelete = (req, res = response) => {
-    res.json({
-        msg: 'delete API - controlador'
-    })
+const usuariosDelete = async(req, res = response) => {
+
+    const { id } = req.params
+
+    // Fisicamente lo borramos
+    //const usuario = await Usuario.findByIdAndDelete( id )
+
+    const usuario = await Usuario.findByIdAndUpdate( id, { estado: false } )
+
+
+    res.json( usuario )
 }
 
 
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,7 +1,7 @@
 const { Router } = require('express')
 const { check } = require('express-validator')
 
-const { esRoleValido, emailExiste } = require('../helpers/db-validators')
+const { esRoleValido, emailExiste, existeUsuarioPorId } = require('../helpers/db-validators')
 const { validarCampos } = require('../middlewares/validar-campos')
 
 const { usuariosGet, 
@@ -15,7 +15,12 @@ const router = Router()
 
 router.get('/', usuariosGet )
 
-router.put('/:id', usuariosPut )
+router.put('/:id', [
+        check('id', 'No es un ID válido').isMongoId(),
+        check('id').custom( existeUsuarioPorId ),
+        check('rol').custom( esRoleValido ),
+        validarCampos
+],usuariosPut )
 
 router.post('/', [
         check('nombre', 'El nombre es obligatorio').not().isEmpty(),
@@ -26,7 +31,11 @@ router.post('/', [
         validarCampos
 ] ,usuariosPost )
 
-router.delete('/', usuariosDelete )
+router.delete('/:id', [
+        check('id', 'No es un ID válido').isMongoId(),
+        check('id').custom( existeUsuarioPorId ),
+        validarCampos
+], usuariosDelete )
 
 router.patch('/', usuariosPatch )
 
