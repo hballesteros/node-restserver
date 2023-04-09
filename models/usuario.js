b84247adad92b2636a8143a69e295702b35b33f6
Hugo <hugo@boahstudio.com>
2023-04-09 02:15:36 -0300

    Google Signin - version 4.0.0

--- models/usuario.js
+++ models/usuario.js
@@ -21,7 +21,9 @@ const UsuarioSchema = Schema({
     },
     rol: {
         type: String,
-        required: true
+        required: true,
+        default: 'USER_ROLE',
+        emun: ['ADMIN_ROLE', 'USER_ROLE']
     },
     estado: {
         type: Boolean,
