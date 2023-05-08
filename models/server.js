4ef707eb9c0b0516f33467eab105f9f687257983
Hugo <hugo@boahstudio.com>
2023-05-08 00:52:20 -0300

    upload files setup

--- models/server.js
+++ models/server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const cors = require('cors')
+const fileUpload = require('express-fileupload')
+
 const { dbConnection } = require('../database/config')
 
 
@@ -15,6 +17,7 @@ class Server {
             categorias: '/api/categorias',
             productos:  '/api/productos',
             usuarios:   '/api/usuarios',
+            uploads:   '/api/uploads',
         }
         
         // Conectar a base de datos
@@ -43,6 +46,13 @@ class Server {
         // Directorio Público
         this.app.use( express.static('public') )
 
+        // Fileuplad - Carga de archivos
+        this.app.use(fileUpload({
+            useTempFiles: true,
+            tempFileDir: '/tmp/',
+            createParentPath: true
+        }));
+
     }
 
     routes() {
@@ -52,6 +62,7 @@ class Server {
         this.app.use(this.paths.categorias, require('../routes/categorias'))
         this.app.use(this.paths.productos, require('../routes/productos'))
         this.app.use(this.paths.usuarios, require('../routes/usuarios'))
+        this.app.use(this.paths.uploads, require('../routes/uploads'))
     
     }
 
