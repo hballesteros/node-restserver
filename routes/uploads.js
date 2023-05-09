181c5aac34c2cb39e70e0531a7efd9a3a6b1da81
Hugo <hugo@boahstudio.com>
2023-05-09 13:00:56 -0300

    Cloudinary

--- routes/uploads.js
+++ routes/uploads.js
@@ -2,7 +2,10 @@ const { Router } = require('express')
 const { check } = require('express-validator')
 
 const { validarCampos, validarArchivoSubir } = require('../middlewares')
-const { cargarArchivo, actualizarImagen } = require('../controllers/uploads')
+const { cargarArchivo, 
+        actualizarImagen, 
+        mostrarImagen, 
+        actualizarImagenCloudinary } = require('../controllers/uploads')
 const { coleccionesPermitidas } = require('../helpers')
 
 const router = Router()
@@ -16,7 +19,14 @@ router.put( '/:coleccion/:id', [
     check('id','El is debe ser de mongo').isMongoId(),
     check('coleccion').custom( c => coleccionesPermitidas( c, ['usuarios','productos'])),
     validarCampos
-], actualizarImagen )
+], actualizarImagenCloudinary )
+//], actualizarImagen )
+
+router.get('/:coleccion/:id', [
+    check('id','El is debe ser de mongo').isMongoId(),
+    check('coleccion').custom( c => coleccionesPermitidas( c, ['usuarios','productos'])),
+    validarCampos
+], mostrarImagen)
 
 
 
